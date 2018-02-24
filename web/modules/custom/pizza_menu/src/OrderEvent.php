3d3f7b08e686d113408f820e1e70d22620bd1b13
ion.cracea <ion.cracea@gmail.com>
2018-02-24 15:35:26 +0200

    OrderEvent class

---
+++ web/modules/custom/pizza_menu/src/OrderEvent.php
@@ -0,0 +1,40 @@
+<?php
+namespace Drupal\pizza_menu;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class OrderEvent extends Event {
+
+  /**
+   * Changed order status
+   */
+  const STATUS = 'event.order.status';
+
+  /**
+   * Order Add
+   */
+  const ADD = 'event.order.add';
+
+  /**
+   * Order Update
+   */
+  const UPDATE = 'event.order.update';
+
+
+  protected $referenceID;
+
+  public function __construct()
+  {
+//    $this->referenceID = $referenceID;
+  }
+
+  public function getReferenceID()
+  {
+//    return $this->referenceID;
+  }
+
+  public function myEventDescription() {
+    return "This is as an example event";
+  }
+
+}
