55c345a09b161c0d7fbb194078fcdc90e6896f30
Vlad Moyseenko <vlad.dancer@gmail.com>
2018-02-27 03:50:37 +0200

    Add wodby.yml

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git remote -v
-rev=$(git rev-parse --short HEAD)
-git config user.name "Vlad Moyseenko"
-git config user.password ${GITHUB_PASS}
-git add .
-git commit -m "travis commit at ${rev}"
-git push origin master
